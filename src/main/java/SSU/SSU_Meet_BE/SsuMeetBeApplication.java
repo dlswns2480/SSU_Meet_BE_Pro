e80922351675fde3c2cdc4549e83f1c107cd66cd
M1nseokSong <minseok3206@gmail.com>
2023-06-25 17:07:17 +0900

    First Commit

---
+++ src/main/java/SSU/SSU_Meet_BE/SsuMeetBeApplication.java
@@ -0,0 +1,13 @@
+package SSU.SSU_Meet_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuMeetBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuMeetBeApplication.class, args);
+	}
+
+}
